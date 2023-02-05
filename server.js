f9d5b3206488e80c46553fdcc05bf91d405b3bdf
Alexis2204 <alex.richard220499@gmail.com>
2023-02-05 09:38:32 +0100

    ready for prod

--- server.js
+++ server.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const cors = require('cors');
 const { createProxyMiddleware } = require('http-proxy-middleware');
+require('dotenv').config()
 const app = express();
 
 const options = {
   target: 'https://plan-tan.fr/',
   changeOrigin: true,
+  pathRewrite: {'^/api' : ''},
   onProxyReq: function(proxyReq, req, res) {
     console.log(`Request made to target: ${proxyReq.path}`);
-    }
+  }
 };
 
-app.use(cors({
-  origin: '*'
-}));
-app.use('/', createProxyMiddleware(options));
+app.use(cors({ origin: '*'}));
+app.use(express.static('dist'));
+app.use('/api', createProxyMiddleware(options));
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log('Proxy server running on port 3000');
 });
